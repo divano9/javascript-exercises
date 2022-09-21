d79c85266bdf589db101e0d876fabd0055738b63
Divano-home <dusanivanovic@mail.com>
2022-09-21 16:40:03 +0200

    second task finnished

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,19 @@
-const repeatString = function() {
+const repeatString = function(string,num) {
+    let theString ="";
 
+    if(num < 0){
+        theString ="ERROR"
+    }
+    else{
+        for (let i = 1; i<=num; i++){
+            theString += string
+        }
+    }
+    
+    return theString
 };
+
+// repeatString("hey",3);
 
 // Do not edit below this line
 module.exports = repeatString;
