920ce1e08efb2aaddb0db685442d090d5ff61ab2
Divano-home <dusanivanovic@mail.com>
2022-09-24 00:10:42 +0200

    finnished sixth exercise

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,24 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    
+    leapY = "";
 
+    if(year % 100 === 0){
+        if(year % 400 === 0){
+            leapY = true;
+        }
+        else{
+            leapY = false;
+        }
+    }else if (year % 4 === 0){
+        leapY = true;
+    }
+    else{
+        leapY = false;
+    }
+
+    return leapY
 };
+
 
 // Do not edit below this line
 module.exports = leapYears;
