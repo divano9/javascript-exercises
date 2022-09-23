99195a8ce23a0f0b87f747d4efbe903f9388f74b
Divano-home <dusanivanovic@mail.com>
2022-09-24 00:00:50 +0200

    fifth task done

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,30 @@
-const sumAll = function() {
+const sumAll = function(a,b) {
 
+    let sum = 0;
+
+    if (typeof a === "number" && typeof b === "number" && a > 0 && b > 0 ){
+        if(a < b){
+            for (let i = a; i <= b; i++){
+                
+                sum = sum + i;  
+            }
+        }
+         else{
+            for (let i = b; i <= a; i++){
+                
+                sum += i;
+            }
+        }
+    }
+    else{
+
+        sum = "ERROR";
+    }
+
+    return sum; 
 };
+
+
 
 // Do not edit below this line
 module.exports = sumAll;
