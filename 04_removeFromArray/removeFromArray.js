2ca991ae6b6927fe6963ad5fad3d0274a05c9cf0
Divano-home <dusanivanovic@mail.com>
2022-09-23 23:21:06 +0200

    fourth exercise finnished

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,26 @@
-const removeFromArray = function() {
+const removeFromArray = function(arrayF,...args) {
+    
+    let array = arrayF;
 
-};
+    for (const arg of args){
+        
+
+        let index = array.indexOf(arg);
+       
+        if(array.includes(arg)){
+            
+            array.splice(index,1); 
+        } 
+
+    }
+    
+
+
+    return array
+}; 
+
+
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
