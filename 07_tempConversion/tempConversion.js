dd39d5badb3eebea86c1048d713a9306078c1669
Divano-home <dusanivanovic@mail.com>
2022-09-24 23:03:06 +0200

    6th and 7th exercises finnished

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,10 +1,16 @@
-const ftoc = function() {
+const ftoc = function(f) {
 
+  const c = (f - 32) * (5/9);
+  return Math.round(c * 10) / 10;
 };
 
-const ctof = function() {
+const ctof = function(c) {
 
+  const f = c * (9/5) + 32;
+  return Math.round(f * 10) / 10;
 };
+
+// return Math.round(num * 100) / 100;  two decimals
 
 // Do not edit below this line
 module.exports = {
--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,55 @@
-const add = function() {
+const add = function(a,b) {
+
+  const add = a + b;
+  return add;
+};
+
+const subtract = function(a,b) {
+
+	const subtract = a - b;
+  return subtract
+};
+
+const sum = function(numbs) {
+
+	let sum = 0;
+  for (const numb of numbs){
+    sum += numb;
+  }
+
+  return sum;
+};
+
+const multiply = function(numbs) {
+  
+  multySum = 1;
+  for (const numb of numbs){
+    multySum *= numb;
+  }
+
+  return multySum;
+};
+
+const power = function(a,b) {
+
+	const pow = Math.pow(a,b);
+  return pow;
+};
+
+const factorial = function(a) {
 	
+  let factorial = 1;
+  if (a > 0){
+    for (i = 1; i <= a; i++){
+      factorial *=i;
+    }
+  }
+
+  return factorial;
 };
 
-const subtract = function() {
-	
-};
+// document.getElementById("p").innerHTML = factorial(10);
 
-const sum = function() {
-	
-};
-
-const multiply = function() {
-
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
-};
 
 // Do not edit below this line
 module.exports = {
