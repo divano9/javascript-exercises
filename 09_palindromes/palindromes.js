b17dc5c682eb47340077c3bf37beca6c7c954bda
Divano-home <dusanivanovic@mail.com>
2022-09-29 20:50:40 +0200

    finnished with the 9th task (palindromes)

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,20 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    
+    const nString = string.replace(/\W/g, '').toLowerCase(); // delete all non alphabet(letter) and transform string to lower case
+    let rString = "";
+    for (i=nString.length - 1; i >=0 ; i-- ){
+        rString += nString[i];
+    }
 
+    return nString == rString ? true : false; // if both are equeal its a palindrome, return true, if not return false
+    
 };
+
+
+
+//document.getElementById("p").innerHTML=palindromes("aha!!!");
+
+
 
 // Do not edit below this line
 module.exports = palindromes;
