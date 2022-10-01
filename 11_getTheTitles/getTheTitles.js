103311fb48650264c6360c04b9e688174f3008c8
Divano-home <dusanivanovic@mail.com>
2022-10-01 23:09:35 +0200

    exercise 12 (findTheOldest) finnished, used the reduce method

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,28 @@
-const getTheTitles = function() {
+const getTheTitles = function(books) {
 
+    let title = []; // result is suposed to be an array
+
+    for (const book of books){ // get the titles for each object in an array
+        title.push(book.title);
+    }
+
+    // console.log(title);
+
+    return title
 };
+
+const boks = [
+    {
+      title: 'Book',
+      author: 'Name'
+    },
+    {
+      title: 'Book2',
+      author: 'Name2'
+    }
+  ]
+
+  // document.getElementById("p").innerHTML = getTheTitles(boks);
 
 // Do not edit below this line
 module.exports = getTheTitles;
--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,34 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    
+    const oldest = people.reduce((prev, current) => { // initiates the getAge function, compares the ages of previous and current person
+        
+        const pAge = getAge(prev)
+        const cAge = getAge(current);
+        
+        if (pAge  > cAge) {
+          return prev
+        } else{
+          return current
+        } 
+        
+    },0);
 
+    // return JSON.stringify(oldest) 
+
+    return oldest
 };
+
+function getAge (per){  
+  if (per.yearOfDeath) { // if key yearOfDeath exists. calculate normaly, if not get current date instead of yearOfDeath
+    return per.yearOfDeath - per.yearOfBirth
+  } else{
+    return (new Date).getFullYear() - per.yearOfBirth;
+  } 
+} 
+
+
+
+// document.getElementById("p").innerHTML = findTheOldest(ppl);
 
 // Do not edit below this line
 module.exports = findTheOldest;
