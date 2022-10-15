db67ae0f04d8c3d96a837d25812e84eb66b7f2e0
Divano-home <dusanivanovic@mail.com>
2022-10-15 23:51:11 +0200

    corrected and finnished exercise 13

--- 13_caesar/caesar.js
+++ 13_caesar/caesar.js
@@ -1,47 +1,49 @@
 const caesar = function(letter,num) {
-    const alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-    let text = "";
-    const letterA = letter.split("");
-    console.log(letterA)
+
+    const alphabetL = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]; // lower case alphabet
+    const alphabetU = alphabetL.map(element => {   // upper case alphabet
+        return element.toUpperCase();
+      });
+    let text = ""; // final output
+    let char ="";  // character from parameter letter
+    const letterA = letter.split(""); // parameter letter split into array
+
     for (let i = 0; i < letter.length; i++){ 
-        const char = letterA[i].toLowerCase();
-        
-        if(char == " "){  // if whitespace add it without changing
-            text += " ";
-        }else if(char == ","){
-            text += ",";
-        }else if(char == "!"){
-            text += "!";
-        }else {
-         const charIndex = alphabet.indexOf(char); // find requested letter in alphabet array and change it according to num
-         // text += alphabet[charIndex + num] 
-            if((num) > (alphabet.length - 1)){
-            const times = Math.floor(num / (alphabet.length - 1));
-            
-            const aIndex = ((num % 25) + 25) % charIndex;
-            text += alphabet.at(aIndex);
-            console.log("char: ", char, " new index: ", aIndex, num - times*charIndex,alphabet.length - 1);
-            }else{
-                text += alphabet.at(charIndex + num);
-                console.log(char, charIndex, letter)
-            
+
+        char = letterA[i];
+
+         if( char.toUpperCase() != char.toLowerCase() ){ // if char is a letter (alphabet)
+    
+            if(alphabetU.includes(char)){ // if char is in upper case
+
+                const charIndex = alphabetU.indexOf(char); // find the alphabetU array index of matching character (that equals char)
+                const aIndex = (charIndex + (num % 26)) % 26; // formula that shortens the requested number (num) to within the array range
+                text += alphabetU.at(aIndex); // add the matching letter to output
+              
+
+            } else{ // char is in lower case
+
+                const charIndex = alphabetL.indexOf(char); // find the alphabetL array index of matching character (that equals char)
+                const aIndex = (charIndex + (num % 26)) % 26; // formula that shortens the requested number (num) to within the array range
+                text += alphabetL.at(aIndex); // add the matching letter to output
+               
             }
+
+         } else{ // if character (char) is not a letter, add it as is
+             
+             text += char;
+         }
          
          
         }
         
-    }
 
-    return text.replace(/(^\w|\s\w)/g, m => m.toUpperCase());
+    return text;
+
 };
 
-const al = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","v","v","w","x","y","z"];
-console.log(al.indexOf("c"))
 
- //  document.getElementById("p").innerHTML = al.at(1 + -2);
-
-
-document.getElementById("p").innerHTML = caesar('Hello, World!', 75);
+// document.getElementById("p").innerHTML = caesar('Hello, World!', 75);
 
 // Do not edit below this line
 module.exports = caesar;
